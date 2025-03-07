313db1efbdce8936f624bf9546f2d6ecdc3df7f4
LU SIFE <62038653+LU-SIFE@users.noreply.github.com>
2025-03-06 18:51:29 -0700

    removed weird highlighting

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 var HyperMD = require("hypermd");
 
-// Load these modes if you want highlighting ...
-require("codemirror/mode/htmlmixed/htmlmixed") // for embedded HTML
-require("codemirror/mode/stex/stex") // for Math TeX Formular
-require("codemirror/mode/yaml/yaml") // for Front Matters
-
 // Load PowerPacks if you want to utilize 3rd-party libs
 require("hypermd/powerpack/fold-math-with-katex") // implicitly requires "katex"
 require("hypermd/powerpack/hover-with-marked") // implicitly requires "marked"
